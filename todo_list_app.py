a2acf04a9ce1c012cd917f8b0e7d25f3754a1517
Forlaenu <93723560+Forlaenu@users.noreply.github.com>
2021-11-16 07:10:33 -0600

    first commit

---
+++ todo_list_app.py
@@ -0,0 +1,32 @@
+# In this exercise you are going to create a Todo List App. 
+# Create a new folder and initialize it as a git repository. 
+# Connect the repository up to github and make sure you commit and 
+# push your changes as you work through the solution.
+
+# Features:
+# When the app starts it should present user with the following menu:
+
+# Press 1 to add task
+# Press 2 to delete task
+# Press 3 to view all tasks
+# Press q to quit
+# : 
+# The user should only be allowed to quit when they press 'q'.
+
+# Add Task:
+# Ask the user for the 'title' and 'priority' of the task. Priority can be high, medium and low. Make sure you validate this.
+
+# Delete Task:
+# Show user all the tasks along with the index number of each task. User can then enter the index number of the task to delete the task.
+
+# View all tasks:
+# Allow the user to view all the tasks in the following format:
+
+# 1 - Wash the car - high
+# 2 - Mow the lawn - low
+
+# Implementation Suggestions
+# Store each task in a dictionary and use 'title' and 'priority' as keys of the dictionary.
+
+# Store each dictionary inside an array. Array will represent list of tasks.
+
